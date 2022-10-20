90f596883f4eca2d9f163d109b0f3d7c6e0ac24e
Markis Kelly <Markis.Kelly@magaero.com>
2022-10-20 19:17:21 -0400

    add projectId for end to end

--- applications/redeye-e2e/cypress.config.js
+++ applications/redeye-e2e/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+	projectId: 'szqro4',
 	fixturesFolder: './src/fixtures',
 	modifyObstructiveCode: false,
 	video: false,
