e98a7f851a54852c8b12ef87e3fdd851d7b0744a
Austin Golding <austin.golding.dev@icloud.com>
2023-01-05 21:51:14 -0800

    Add custom auth middleware for global operator graphql query and mutation Update login form with feedback that creating a new user is disabled Closes #55

--- applications/server/src/store/operator-resolvers.ts
+++ applications/server/src/store/operator-resolvers.ts
@@ -1,6 +1,6 @@
 import { beaconHidden, defaultHidden } from './utils/hidden-entities-helper';
 import { connectToProjectEmOrFail, getMainEmOrFail } from './utils/project-db';
-import { Resolver, Query, Arg, Mutation, Ctx } from 'type-graphql';
+import { Resolver, Query, Arg, Mutation, Ctx, Authorized, createMethodDecorator, ResolverData } from 'type-graphql';
 import { Operator, GlobalOperator } from '@redeye/models';
 import { RelationPath } from './utils/relation-path';
 import type { Relation } from './utils/relation-path';
@@ -8,6 +8,7 @@ import type { GraphQLContext } from '../types';
 
 @Resolver(Operator)
 export class OperatorResolvers {
+	@Authorized()
 	@Query(() => [Operator], { nullable: 'itemsAndList', description: 'Get all the operators for a project' })
 	async operators(
 		@Ctx() ctx: GraphQLContext,
@@ -24,19 +25,23 @@ export class OperatorResolvers {
 		return operators;
 	}
 
+	@PasswordAuth()
 	@Query(() => [GlobalOperator], { nullable: 'itemsAndList', description: 'Get all the operators for all campaigns' })
 	async globalOperators(
 		@Ctx() ctx: GraphQLContext,
-		@RelationPath() relationPaths: Relation<GlobalOperator>
+		@RelationPath() relationPaths: Relation<GlobalOperator>,
+		@Arg('password', () => String) _password: string
 	): Promise<GlobalOperator[]> {
 		const em = getMainEmOrFail(ctx);
 		return await em.find(GlobalOperator, {}, { populate: relationPaths });
 	}
 
+	@PasswordAuth()
 	@Mutation(() => GlobalOperator, { nullable: true, description: 'Create a global user' })
 	async createGlobalOperator(
 		@Ctx() ctx: GraphQLContext,
-		@Arg('username', () => String) username: string
+		@Arg('username', () => String) username: string,
+		@Arg('password', () => String) _password: string
 	): Promise<GlobalOperator | null> {
 		return await OperatorResolvers.createGlobalOperatorHandler(ctx, username);
 	}
@@ -51,4 +56,12 @@ export class OperatorResolvers {
 		}
 		throw new Error('Operator already exists');
 	}
+}
+
+function PasswordAuth() {
+	return createMethodDecorator(async ({ args, context }: ResolverData<GraphQLContext>, next) => {
+		if (!context.config.blueTeam && (!args.password || args.password !== context.config.password))
+			throw Error('Not Authorized');
+		return next();
+	});
 }
--- applications/server/src/store/progress-resolvers.ts
+++ applications/server/src/store/progress-resolvers.ts
@@ -1,8 +1,9 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Authorized } from 'type-graphql';
 import { ParsingProgress } from '@redeye/models';
 
 @Resolver(ParsingProgress)
 export class ProgressResolvers {
+	@Authorized()
 	@Query(() => ParsingProgress, { description: 'Get the current progress of the parser' })
 	async parsingProgress(): Promise<ParsingProgress> {
 		return { progress: [], nextTaskDescription: "I'm broken", date: new Date() };
