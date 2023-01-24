01048069605c4946e85cf982731244fe9829bac4
Sebastian Ang <22081109+sang2925@users.noreply.github.com>
2023-01-24 10:53:46 -0800

    Cypress Upload Logs Test - Red Team (#69)
    * upload raw logs using graphql
    
    * remove playwright
    
    * update upload log test
    
    * remove unused code
    
    * fix webpack compilation error cypress
    
    * video on failure only
    
    * delete campagin

--- applications/redeye-e2e/cypress.config.js
+++ applications/redeye-e2e/cypress.config.js
@@ -3,11 +3,10 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
 	fixturesFolder: './src/fixtures',
 	modifyObstructiveCode: false,
-	video: false,
+	videoUploadOnPasses: false,
 	videosFolder: '../../dist/applications/redeye-e2e/videos',
 	screenshotsFolder: '../../dist/applications/redeye-e2e/screenshots',
 	failOnStatusCode: false,
-	experimentalWebKitSupport: true,
 	viewportWidth: 1920,
 	viewportHeight: 1080,
 	reporter: '../../node_modules/cypress-multi-reporters',
@@ -19,7 +18,6 @@ module.exports = defineConfig({
 	},
 	e2e: {
 		setupNodeEvents(on, config) {},
-		experimentalSessionAndOrigin: true,
 		specPattern: '../../**/*.cy.js',
 		supportFile: './src/support/index.js',
 		excludeSpecPattern: '*.skip.js',
