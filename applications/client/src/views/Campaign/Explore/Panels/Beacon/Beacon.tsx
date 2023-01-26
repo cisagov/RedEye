7569cf0f9557985d3b4fd54a0faaf93f3ceed277
Li, Huiqing <75810860+sharplessHQ@users.noreply.github.com>
2023-01-25 19:57:39 -0800

    Quick metadata editing for Server/Host/Beacon (#72)

--- applications/client/src/views/Campaign/Explore/Panels/Beacon/Beacon.tsx
+++ applications/client/src/views/Campaign/Explore/Panels/Beacon/Beacon.tsx
@@ -1,14 +1,16 @@
 import { Classes } from '@blueprintjs/core';
+import { ViewOff16 } from '@carbon/icons-react';
 import { dateShortFormat, semanticIcons } from '@redeye/client/components';
 import type { BeaconModel } from '@redeye/client/store';
 import { useStore } from '@redeye/client/store';
 import { TimeStatus } from '@redeye/client/types/timeline';
-import { IconLabel, InfoRow, RowMuted, RowTime, RowTitle } from '@redeye/client/views';
+import { IconLabel, InfoRow, RowMuted, RowTime, RowTitle, useToggleHidden } from '@redeye/client/views';
 import { FlexSplitter } from '@redeye/ui-styles';
 import { observer } from 'mobx-react-lite';
 import type { ComponentProps } from 'react';
 import { useMemo } from 'react';
 import { MitreTechniqueIcons } from '../../components/MitreTechniqueIcons';
+import { QuickMeta } from '../QuickMeta';
 
 type BeaconProps = ComponentProps<'div'> & {
 	beacon: BeaconModel;
@@ -21,6 +23,13 @@ export const BeaconRow = observer<BeaconProps>(({ beacon, ...props }) => {
 		[beacon.displayName, beacon.server?.displayName]
 	);
 
+	const [, mutateToggleHidden] = useToggleHidden(
+		async () =>
+			await store.graphqlStore.mutateToggleBeaconHidden({
+				campaignId: store.campaign?.id!,
+				beaconId: beacon?.id!,
+			})
+	);
 	return (
 		<InfoRow
 			cy-test="info-row"
@@ -36,6 +45,7 @@ export const BeaconRow = observer<BeaconProps>(({ beacon, ...props }) => {
 			</RowTime>
 			<RowTitle className={skeletonClass}>{beacon?.displayName || `${beacon.server?.displayName}`}</RowTitle>
 			<RowMuted className={skeletonClass}>{beacon.meta?.[0]?.maybeCurrent?.username}</RowMuted>
+			{beacon?.hidden && <IconLabel title="Hidden" icon={ViewOff16} />}
 			<FlexSplitter />
 			<MitreTechniqueIcons mitreAttackIds={beacon.mitreTechniques} />
 			<IconLabel
@@ -45,6 +55,7 @@ export const BeaconRow = observer<BeaconProps>(({ beacon, ...props }) => {
 				icon={semanticIcons.commands}
 				className={skeletonClass}
 			/>
+			<QuickMeta modal={beacon} mutateToggleHidden={mutateToggleHidden} />
 		</InfoRow>
 	);
 });
