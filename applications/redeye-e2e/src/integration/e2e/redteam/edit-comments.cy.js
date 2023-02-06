2f12c52e72a19693b5ec026fb58bfeb2ee2e47cd
Sebastian Ang <angdajiansebastian@gmail.com>
2023-02-06 14:27:29 -0800

    updating campaign comments

---
+++ applications/redeye-e2e/src/integration/e2e/redteam/edit-comments.cy.js
@@ -0,0 +1,105 @@
+/// <reference types="cypress" />
+
+describe('Campaign comments', () => {
+	const editedComment = 'Edited comment text';
+	const tag = 'testing';
+	const tag1 = 'test_1';
+	const tag2 = 'test_2';
+
+	it('Add comment and edit the text afterward', () => {
+		// Open the campaign and add a new comment
+		cy.selectCampaign(camp);
+
+		cy.clickCommandTypesTab();
+
+		cy.selectCommandType(cmd);
+
+		cy.addNewComment('0', comment, tag2);
+
+		cy.get('[cy-test=add-comment]').eq(0).scrollIntoView().should('be.visible').click({ force: true });
+
+		cy
+			.get('[cy-test=existing-comment-display]')
+			.scrollIntoView()
+			.should('be.visible')
+			.and('contain', comment)
+			.and('contain', tag2);
+
+		cy.get('[cy-test=add-comment]').eq(0).click({ force: true });
+
+		// After saving comment, edit the comment text
+		cy.editExistingComment(0, editedComment);
+
+		// Verify edited text was saved
+		cy.get('[cy-test=add-comment]').eq(0).should('be.visible').click({ force: true });
+
+		cy.get('[cy-test=existing-comment-display]').should('be.visible').and('contain', editedComment).and('contain', tag2);
+	});
+
+	// SKIPPING FOR NOW - PENDING BUG FIX (https://jira.pnnl.gov/jira/browse/BLDSTRIKE-544). Note. using it.skip makes the entire test fail, so commenting out insteadyarn
+	it('Reply to a comment and verify comment count increases', () => {
+		cy.searchForCampaign(camp);
+
+		// Log the total number of comments showing on the campaign card
+		cy.get('[cy-test=comment-count]').then((cardCount1) => {
+			const campaignCardCount1 = cardCount1.text().split(' ').shift();
+			cy.get('[cy-test=comment-count]').should('contain', campaignCardCount1);
+
+			// Open campaign and verify Presentation mode shows the same number of comments; log number with Golden Ticket tag
+			cy.selectCampaign(camp);
+
+			cy.clickPresentationMode();
+
+			cy
+				.get('[cy-test=all] [cy-test=count]')
+				.should('be.visible')
+				.invoke('text')
+				.then((commentCountAll1) => {
+					cy.log(commentCountAll1);
+					expect(commentCountAll1).to.eq(campaignCardCount1);
+
+					// Go to Comments and verify total there matches
+					cy.clickExplorerMode();
+
+					cy.clickCommentsTab();
+					cy
+						.get('[cy-test=comment-group]')
+						.its('length')
+						.then((commentsTab1) => {
+							expect(+commentsTab1).to.eq(+commentCountAll1).and.to.eq(+campaignCardCount1);
+
+							// Reply to one of the comments with a comment and a tag
+							cy.replyToComment(0, 'Replying to above comment.');
+							cy.addExistingTagsToReply('Golden');
+
+							// Go back to Presentation mode and verify that the comment count and Golden Ticket count increased by one
+							cy.clickPresentationMode();
+
+							cy
+								.get('[cy-test=all] [cy-test=count]')
+								.should('be.visible')
+								.invoke('text')
+								.then((commentCountAll2) => {
+									cy.log(commentCountAll2);
+									expect(+commentCountAll2)
+										.to.eq(+commentCountAll1 + 1)
+										.and.to.eq(+campaignCardCount1 + 1);
+
+									// Go back to campaign card and verify that the comment count increase by one
+									cy.returnToCampaignCard();
+									cy.searchForCampaign(camp);
+
+									cy.get('[cy-test=comment-count]').then((cardCount2) => {
+										const campaignCardCount2 = cardCount2.text().split(' ').shift();
+										cy.get('[cy-test=comment-count]').should('contain', campaignCardCount2);
+
+										expect(+campaignCardCount2)
+											.to.eq(+campaignCardCount1 + 1)
+											.and.to.eq(+commentCountAll2);
+									});
+								});
+						});
+				});
+		});
+	});
+});
