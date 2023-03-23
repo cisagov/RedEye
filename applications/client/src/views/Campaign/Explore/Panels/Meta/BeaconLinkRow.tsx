7dfe9a6548ab0abf8da5595c37cae8085f1c6556
James Bradford <j.bradford@pnnl.gov>
2023-03-22 18:24:19 -0700

    update BeaconLinkRow layout

--- applications/client/src/views/Campaign/Explore/Panels/Meta/BeaconLinkRow.tsx
+++ applications/client/src/views/Campaign/Explore/Panels/Meta/BeaconLinkRow.tsx
@@ -2,8 +2,8 @@ import { Classes } from '@blueprintjs/core';
 import { css } from '@emotion/react';
 import { CarbonIcon, semanticIcons } from '@redeye/client/components';
 import type { LinkModel } from '@redeye/client/store';
-import type { FlexProps } from '@redeye/ui-styles';
 import { CoreTokens, Flex, FlexSplitter, Txt } from '@redeye/ui-styles';
+import type { FlexProps } from '@redeye/ui-styles';
 import { observer } from 'mobx-react-lite';
 import { NavBreadcrumbs } from '../../components';
 
@@ -13,10 +13,10 @@ export type BeaconLinkRowProps = FlexProps & {
 };
 
 export const BeaconLinkRow = observer<BeaconLinkRowProps>(({ direction, link, ...props }) => (
-	<Flex gap={4} align="center" {...props}>
-		<Flex gap={4} align="center" css={{ width: 60 }}>
+	<Flex gap={8} align="center" {...props}>
+		<Flex gap={4} align="center" css={{ flexBasis: 60, color: CoreTokens.TextMuted }}>
 			<CarbonIcon icon={direction === 'To' ? semanticIcons.linkTo : semanticIcons.linkFrom} />
-			<Txt disabled>{direction}</Txt>
+			<Txt>{direction}</Txt>
 		</Flex>
 		<NavBreadcrumbs
 			muted
@@ -24,11 +24,18 @@ export const BeaconLinkRow = observer<BeaconLinkRowProps>(({ direction, link, ..
 			beacon={direction === 'To' ? link.destination?.current : link.origin?.current}
 			css={navBreadCrumbsStyle}
 		/>
-		<FlexSplitter />
-		{link.command?.current && (
+		{/* <Spacer>/</Spacer> */}
+		{link.command?.current ? (
 			<Txt muted>{link.command.current.inputText}</Txt>
-			// : ( <Txt disabled italic>unknown</Txt> )}
+		) : (
+			<Txt disabled italic>
+				unknown
+			</Txt>
 		)}
+		<FlexSplitter />
+		{/* TODO: comment related to link...
+			<HeroButton children={<CarbonIcon cy-test="add-comment" icon={semanticIcons.comment} />} /> 
+		*/}
 	</Flex>
 ));
 
