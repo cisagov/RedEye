da5ecf41f1930ebc25f5a70c5925dc5c18651afa
James Bradford <j.bradford@pnnl.gov>
2023-03-28 07:44:50 -0700

    get the graph dev mode running again

--- packages/graph/index.js
+++ packages/graph/index.js
@@ -2,21 +2,22 @@ import { json } from 'd3';
 import { RedEyeGraph, getServers } from './src/index.ts';
 
 export const testGraph = (svgElementId) => {
+	// json('./testData/data1.json').then((rawGraphData) => {
 	const randomGraphData = getServers(75, 5);
-	json('./testData/data1.json').then((rawGraphData) => {
-		window.graph = new RedEyeGraph({
-			// onSelectionChange: (node) => console.log(node?.id),
-			// onPreviewChange: (node) => console.log(node),
-			// graphData: randomGraphData,
-			graphData: {
-				...rawGraphData.graph,
-				parents: Object.keys(rawGraphData.hosts).map((hostId) => ({
-					name: rawGraphData.hosts[hostId].hostName,
-					id: hostId,
-				})),
-			},
-			element: document.getElementById('app'),
-		});
-		window.addEventListener('resize', () => window.graph.resize());
+	window.graph = new RedEyeGraph({
+		// onSelectionChange: (node) => console.log(node?.id),
+		// onPreviewChange: (node) => console.log(node),
+		graphData: randomGraphData,
+		// graphData: {
+		// 	...graphData.graph,
+		// 	parents: Object.keys(graphData.hosts).map((hostId) => ({
+		// 		name: graphData.hosts[hostId].hostName,
+		// 		id: hostId,
+		// 	})),
+		// },
+		element: document.getElementById(svgElementId),
 	});
+	window.addEventListener('resize', () => window.graph.resize());
+	console.log(graph);
+	// });
 };
