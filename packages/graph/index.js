1fbdecf230ac638bc4a5c0e60656e78171031629
James Bradford <j.bradford@pnnl.gov>
2023-03-28 09:16:18 -0700

    graph dev mode will start without test data

--- packages/graph/index.js
+++ packages/graph/index.js
@@ -1,23 +1,20 @@
 import { json } from 'd3';
 import { RedEyeGraph, getServers } from './src/index.ts';
 
-export const testGraph = (svgElementId) => {
-	// json('./testData/data1.json').then((rawGraphData) => {
-	const randomGraphData = getServers(75, 5);
+const getGraphData = async () => {
+	try {
+		return await json('./testData/large.json');
+	} catch {
+		return getServers(75, 5);
+	}
+};
+
+export const testGraph = async (svgElementId) => {
+	const graphData = await getGraphData();
 	window.graph = new RedEyeGraph({
-		// onSelectionChange: (node) => console.log(node?.id),
-		// onPreviewChange: (node) => console.log(node),
-		graphData: randomGraphData,
-		// graphData: {
-		// 	...graphData.graph,
-		// 	parents: Object.keys(graphData.hosts).map((hostId) => ({
-		// 		name: graphData.hosts[hostId].hostName,
-		// 		id: hostId,
-		// 	})),
-		// },
+		graphData,
 		element: document.getElementById(svgElementId),
 	});
 	window.addEventListener('resize', () => window.graph.resize());
-	console.log(graph);
-	// });
+	console.log(window.graph);
 };
