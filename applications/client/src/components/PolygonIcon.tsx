7ddb377e9f3641a3873c8c05f6723278cbe3ca7c
James Bradford <j.bradford@pnnl.gov>
2023-03-31 15:40:56 -0700

    PolygonIcon

--- applications/client/src/components/PolygonIcon.tsx
+++ applications/client/src/components/PolygonIcon.tsx
@@ -1,7 +1,6 @@
 import { Classes } from '@blueprintjs/core';
-import { css } from '@emotion/react';
 import type { PolygonShapeEx } from '@redeye/graph';
-import { polygonShapePointsOpticallyEqualized, polygonShapePoints } from '@redeye/graph';
+import { polygonShapePointsOpticallyEqualized } from '@redeye/graph';
 import { observer } from 'mobx-react-lite';
 import type { ComponentProps } from 'react';
 
@@ -17,13 +16,7 @@ export const PolygonIcon = observer<PolygonIconProps>(({ polygonShape, size = 16
 	const y = polygonShape === 'triangleDown' ? -dy : polygonShape === 'triangleUp' ? dy : 0;
 	return (
 		<span className={[Classes.ICON, className].join(' ')} css={{}} {...props}>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				// xmlns:xlink="http://www.w3.org/1999/xlink"
-				width={size}
-				height={size}
-				viewBox={`0 0 ${size} ${size}`}
-			>
+			<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
 				{polygonShape === 'circle' ? (
 					<circle r={radius} cx={center} cy={center} />
 				) : (
--- applications/client/src/views/Campaign/Explore/Panels/Beacon/Beacon.tsx
+++ applications/client/src/views/Campaign/Explore/Panels/Beacon/Beacon.tsx
@@ -1,6 +1,6 @@
 import { Classes } from '@blueprintjs/core';
 import { ViewOff16 } from '@carbon/icons-react';
-import { CarbonIcon, dateShortFormat, semanticIcons } from '@redeye/client/components';
+import { dateShortFormat, PolygonIcon, semanticIcons } from '@redeye/client/components';
 import type { BeaconModel } from '@redeye/client/store';
 import { useStore } from '@redeye/client/store';
 import { InfoType } from '@redeye/client/types';
@@ -56,7 +56,18 @@ export const BeaconRow = observer<BeaconProps>(({ beacon, ...props }) => {
 				{store.settings.momentTz(beacon.minTime)?.format(dateShortFormat)}&mdash;
 				{store.settings.momentTz(beacon.maxTime)?.format(dateShortFormat)}
 			</RowTime>
+			<PolygonIcon polygonShape="circle" />
+			{/* 
+			<PolygonIcon polygonShape='triangleUp' />
+			<PolygonIcon polygonShape='triangleDown' />
+			<PolygonIcon polygonShape='diamond' />
+			<PolygonIcon polygonShape='square' />
+			<PolygonIcon polygonShape='pentagonUp' />
+			<PolygonIcon polygonShape='pentagonDown' />
+			<PolygonIcon polygonShape='HexagonUp' />
+			<PolygonIcon polygonShape='HexagonDown' /> 
 			<CarbonIcon icon={semanticIcons.beacon} />
+			*/}
 			<RowTitle cy-test="beacon-display-name" className={skeletonClass}>
 				{beacon?.displayName || `${beacon.server?.displayName}`}
 			</RowTitle>
