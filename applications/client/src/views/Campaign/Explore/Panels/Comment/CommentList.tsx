92c5fe9a932fb498b06d1641996d3892a994691d
Huiqing Li <forqing@gmail.com>
2023-05-03 12:03:14 -0700

    useMemo/useCallback for list action/title

--- applications/client/src/views/Campaign/Explore/Panels/Comment/CommentList.tsx
+++ applications/client/src/views/Campaign/Explore/Panels/Comment/CommentList.tsx
@@ -10,6 +10,7 @@ import { useQuery } from '@tanstack/react-query';
 import { observer } from 'mobx-react-lite';
 import { Bookmark16, ChatLaunch16 } from '@carbon/icons-react';
 import { Hashtag, User } from '@carbon/icons-react/next';
+import { useCallback, useMemo } from 'react';
 import { InfoRow, RowTitle, IconLabel } from '../../components';
 
 type CommentListProps = {
@@ -29,35 +30,47 @@ export const OverviewComments = observer<CommentListProps>(({ setCommandGroupIds
 			)
 	);
 
+	const handleClickType = useCallback((presentationItem) => {
+		setCommandGroupIds(presentationItem.commandGroupIds);
+		store.campaign.setOverviewCommentList(
+			presentationItem.id === 'all' && presentationItem.key === 'All Comments'
+				? OverviewCommentList.ALL_COMMENTS
+				: presentationItem.id === 'favorited' && presentationItem.key === 'Favorited Comments'
+				? OverviewCommentList.FAVORITED_COMMENTS
+				: presentationItem.id === 'procedural' && presentationItem.key === 'parser-generated'
+				? OverviewCommentList.PROCEDURAL
+				: presentationItem.id.slice(0, 5) === 'user-' && presentationItem.key.slice(0, 5) === 'user-'
+				? OverviewCommentList.USER_COMMENTS
+				: OverviewCommentList.TAG_COMMENTS
+		);
+		store.campaign.setOverviewCommentType(
+			(presentationItem.id === 'all' && presentationItem.key === 'All Comments') ||
+				(presentationItem.id === 'favorited' && presentationItem.key === 'Favorited Comments') ||
+				(presentationItem.id === 'procedural' && presentationItem.key === 'parser-generated')
+				? presentationItem.key
+				: presentationItem.id.slice(0, 5) === 'user-' && presentationItem.key.slice(0, 5) === 'user-'
+				? presentationItem.id.slice(5)
+				: `#${presentationItem.id}`
+		);
+	}, []);
+
+	const rowTitle = useMemo(
+		() => (presentationItem) =>
+			presentationItem.key[0] === '#'
+				? presentationItem.key.slice(1)
+				: presentationItem.key.slice(0, 5) === 'user-'
+				? presentationItem.key.slice(5)
+				: presentationItem.key,
+		[]
+	);
+
 	return (
 		<>
 			{data?.presentationItems?.map((presentationItem, i) => (
 				<InfoRow
 					// eslint-disable-next-line react/no-array-index-key
 					key={`${presentationItem.id}-${i}`}
-					onClick={() => {
-						setCommandGroupIds(presentationItem.commandGroupIds);
-						store.campaign.setOverviewCommentList(
-							presentationItem.id === 'all' && presentationItem.key === 'All Comments'
-								? OverviewCommentList.ALL_COMMENTS
-								: presentationItem.id === 'favorited' && presentationItem.key === 'Favorited Comments'
-								? OverviewCommentList.FAVORITED_COMMENTS
-								: presentationItem.id === 'procedural' && presentationItem.key === 'parser-generated'
-								? OverviewCommentList.PROCEDURAL
-								: presentationItem.id.slice(0, 5) === 'user-' && presentationItem.key.slice(0, 5) === 'user-'
-								? OverviewCommentList.USER_COMMENTS
-								: OverviewCommentList.TAG_COMMENTS
-						);
-						store.campaign.setOverviewCommentType(
-							(presentationItem.id === 'all' && presentationItem.key === 'All Comments') ||
-								(presentationItem.id === 'favorited' && presentationItem.key === 'Favorited Comments') ||
-								(presentationItem.id === 'procedural' && presentationItem.key === 'parser-generated')
-								? presentationItem.key
-								: presentationItem.id.slice(0, 5) === 'user-' && presentationItem.key.slice(0, 5) === 'user-'
-								? presentationItem.id.slice(5)
-								: `#${presentationItem.id}`
-						);
-					}}
+					onClick={() => handleClickType(presentationItem)}
 				>
 					{presentationItem.id !== 'all' && (
 						<CarbonIcon
@@ -70,11 +83,7 @@ export const OverviewComments = observer<CommentListProps>(({ setCommandGroupIds
 						muted={presentationItem.id === 'procedural'}
 						italic={presentationItem.id === 'procedural'}
 					>
-						{presentationItem.key[0] === '#'
-							? presentationItem.key.slice(1)
-							: presentationItem.key.slice(0, 5) === 'user-'
-							? presentationItem.key.slice(5)
-							: presentationItem.key}
+						{rowTitle(presentationItem)}
 					</RowTitle>
 					<FlexSplitter />
 					<IconLabel title="Commands" value={presentationItem.commandCount} icon={semanticIcons.commands} />
