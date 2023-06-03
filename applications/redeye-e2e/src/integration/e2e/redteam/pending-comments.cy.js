d8f2e2e8681992089af234198eb2184063122268
Courtney Carpenter <ccarpenter28@gmail.com>
2023-06-03 10:52:12 -0700

    Add test to verify pending comments do not persist after clicking Cancel button.

--- applications/redeye-e2e/src/integration/e2e/redteam/pending-comments.cy.js
+++ applications/redeye-e2e/src/integration/e2e/redteam/pending-comments.cy.js
@@ -4,9 +4,30 @@ describe('Pending comments', () => {
 	const camp = 'pendingcomments';
 	const fileName = 'gt.redeye';
 
+	const commentText = 'PENDING COMMENT';
+
 	it('Pending comment should persist when navigating away', () => {
 		// Upload campaign and open
 		cy.uploadCampaign(camp, fileName);
+		cy.selectCampaign(camp);
+
+		// Go to Beacons tab, select a beacon, then start a new comment on a command
+		cy.clickBeaconsTab();
+		cy.get('[cy-test=beacons-row]').eq(0).click();
+		cy.addComment(0, commentText);
+
+		// Navigate away from the current page
+		cy.clickExplorerMode();
+
+		// Navigate back to pending comment and make sure it's still there
+		cy.clickBeaconsTab();
+		cy.get('[cy-test=beacons-row]').eq(0).click();
+		cy.get('[cy-test=add-comment]').eq(0).click();
+		cy.get('[cy-test=comment-input]').should('contain', commentText);
+	});
+
+	it('Pending comment should not persist after clicking Cancel button', () => {
+		// Open campaign
 		cy.selectCampaign(camp);
 
 		// Go to Beacons tab, select a beacon, then start a new comment on a command
@@ -17,11 +38,15 @@ describe('Pending comments', () => {
 		// Navigate away from the current page
 		cy.clickExplorerMode();
 
-		// Navigate back to pending comment and make sure it's still there
+		// Navigate back to pending comment and click Cancel button
 		cy.clickBeaconsTab();
 		cy.get('[cy-test=beacons-row]').eq(0).click();
 		cy.get('[cy-test=add-comment]').eq(0).click();
-		cy.get('[cy-test=comment-input]');
+		cy.get('[cy-test=cancel-comment]').click();
+
+		// Go to Comments tab and verify the comment did not persist
+		cy.clickCommentsTabWithinTab();
+		cy.get('[cy-test=message-row]').should('not.contain', commentText);
 	});
 
 	after(() => {
