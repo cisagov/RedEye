32788fe992c36fc8099c4bc229be86e6b3831425
Li, Huiqing <huiqing.li@pnnl.gov>
2023-06-13 14:51:10 -0700

    astro update

--- applications/landing-page/astro.config.mjs
+++ applications/landing-page/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://sharplesshq.github.io',
+	base: '/RedEye',
+});
