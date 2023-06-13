1788ea6a0058c154a078972061c9b2c8e719d40a
Li, Huiqing <huiqing.li@pnnl.gov>
2023-06-13 15:04:55 -0700

    add yml astro

--- applications/landing-page/astro.config.mjs
+++ applications/landing-page/astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://sharplesshq.github.io',
-	base: '/RedEye',
-});
+export default defineConfig({});
