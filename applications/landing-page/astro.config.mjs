1690791bdfd4e3de87d9035bbe603d87b78f0883
Huiqing Li <forqing@gmail.com>
2023-06-15 12:22:58 -0700

    astro config outdir update

--- applications/landing-page/astro.config.mjs
+++ applications/landing-page/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	// base: '/RedEye', /* use for gh-pages build */
+	outDir: './dist-landing',
 	vite: {
 		build: {
 			rollupOptions: {
