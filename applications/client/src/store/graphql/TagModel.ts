22a4bfe07723aa8478054d72c07d32a6f7751dae
Huiqing Li <forqing@gmail.com>
2023-06-20 15:39:34 -0700

    update tag route in search

--- applications/client/src/store/graphql/TagModel.ts
+++ applications/client/src/store/graphql/TagModel.ts
@@ -1,5 +1,8 @@
-import { ExtendedModel, model } from 'mobx-keystone';
+import { ExtendedModel, model, getRoot, modelAction } from 'mobx-keystone';
 import { TagModelBase } from './TagModel.base';
+import { AppStore } from '../app-store';
+import { routes } from '../routing';
+import { CampaignViews, Tabs } from '@redeye/client/types';
 
 /* A graphql query fragment builders for TagModel */
 export { selectFromTag, tagModelPrimitives, TagModelSelector } from './TagModel.base';
@@ -8,4 +11,19 @@ export { selectFromTag, tagModelPrimitives, TagModelSelector } from './TagModel.
  * TagModel
  */
 @model('Tag')
-export class TagModel extends ExtendedModel(TagModelBase, {}) {}
+export class TagModel extends ExtendedModel(TagModelBase, {}) {
+	@modelAction searchSelect() {
+		const appStore = getRoot<AppStore>(this);
+		appStore.router.updateRoute({
+			path: routes[CampaignViews.EXPLORE],
+			params: {
+				view: CampaignViews.EXPLORE,
+				tab: Tabs.COMMENTS,
+				currentItem: 'comments_list',
+				currentItemId: `tag-${this.id}`,
+				activeItem: undefined,
+				activeItemId: undefined,
+			},
+		});
+	}
+}
