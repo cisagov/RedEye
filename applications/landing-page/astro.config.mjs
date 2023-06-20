d9f834385007e783e50972780b061c0e78e95199
Huiqing Li <75810860+sharplessHQ@users.noreply.github.com>
2023-06-20 14:34:56 -0700

    Landing page (#151)
    * initial landing setup done
    
    * rename User Guide to UserGuide for convenience
    
    * file name case...
    
    * file name case...
    
    * img width/css update
    
    * use github images for landing
    
    * minor update
    
    * remove env
    
    * astro build update for gh-pages setup
    
    * astro config outdir update
    
    * readme update
    
    * remove userGuide

---
+++ applications/landing-page/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	// base: '/RedEye', /* use for gh-pages build */
+	outDir: './dist-landing',
+	vite: {
+		build: {
+			rollupOptions: {
+				output: {
+					assetFileNames: 'assets/style[extname]',
+				},
+			},
+		},
+	},
+});
