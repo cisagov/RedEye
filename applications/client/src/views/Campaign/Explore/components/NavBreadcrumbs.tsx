b95534d5e322b9d3ed5012bc72b3616fbfe41fc6
Huiqing Li <forqing@gmail.com>
2023-07-26 12:28:12 -0700

    fix raw log All route

--- applications/client/src/views/Campaign/Explore/components/NavBreadcrumbs.tsx
+++ applications/client/src/views/Campaign/Explore/components/NavBreadcrumbs.tsx
@@ -171,7 +171,18 @@ export const NavBreadcrumbs = observer<NavBreadcrumbsProps>(
 						onClick: async (e) => {
 							e.stopPropagation();
 							await onNavigate(e);
-							await currentBeacon?.select();
+							store.router.updateRoute({
+								path: routes[CampaignViews.EXPLORE],
+								params: {
+									id: store.campaign.id,
+									view: CampaignViews.EXPLORE,
+									currentItem: 'all',
+									currentItemId: undefined,
+									activeItem: undefined,
+									activeItemId: undefined,
+									tab: Tabs.HOSTS,
+								},
+							});
 						},
 					});
 
