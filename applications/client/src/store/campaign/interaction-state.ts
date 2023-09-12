c67442244912083157cfb4af5acb11370106d602
Huiqing Li <75810860+sharplessHQ@users.noreply.github.com>
2023-09-12 09:06:15 -0700

    Graph node timeline selection (#179)
    * fix host/server graph clicking to highlight timeline
    
    * revert host/items

--- applications/client/src/store/campaign/interaction-state.ts
+++ applications/client/src/store/campaign/interaction-state.ts
@@ -163,8 +163,15 @@ export class InteractionState extends ExtendedModel(() => ({
 			} else if (host) {
 				const comp = this.currentHosts.get(host);
 				if (comp) {
-					for (const hostBeacon of comp.beacons) {
-						this.appStore.campaign.timeline?.setBeacon(hostBeacon);
+					for (const hostBeaconId of comp.beaconIds) {
+						this.appStore.campaign.timeline?.setBeacon(this.currentBeacons.get(hostBeaconId));
+					}
+				}
+			} else if (server) {
+				const comp = this.appStore.graphqlStore.servers.get(server);
+				if (comp) {
+					for (const serverBeacon of comp.beacons) {
+						this.appStore.campaign.timeline?.setBeacon(this.currentBeacons.get(serverBeacon.id));
 					}
 				}
 			}
