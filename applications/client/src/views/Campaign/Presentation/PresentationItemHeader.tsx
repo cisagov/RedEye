01ae690af03e8bf36911d3cdb4591b1aea776cb2
James Bradford <j.bradford@pnnl.gov>
2023-09-19 12:30:19 -0700

    adding more robust PresentationItemHeader

--- applications/client/src/views/Campaign/Presentation/PresentationItemHeader.tsx
+++ applications/client/src/views/Campaign/Presentation/PresentationItemHeader.tsx
@@ -1,7 +1,9 @@
+import { Tooltip } from '@blueprintjs/core';
 import { dateTimeFormat, timeFormat } from '@redeye/client/components';
 import { createState } from '@redeye/client/components/mobx-create-state';
 import { useStore } from '@redeye/client/store';
-import { Flex, Header, Txt } from '@redeye/ui-styles';
+import type { TxtProps } from '@redeye/ui-styles';
+import { Header, Spacer, Txt } from '@redeye/ui-styles';
 import { observer } from 'mobx-react-lite';
 import type { ComponentProps } from 'react';
 
@@ -19,14 +21,28 @@ export const PresentationItemHeader = observer<PresentationItemHeaderProps>(({})
 		},
 	});
 
-	const beacons = store.campaign.presentation.currentSlide?.beacons;
+	const beaconNames = store.campaign.presentation.currentSlide?.beacons.map((beacon) => beacon.computedName);
+
+	const hostNames = Array.from(
+		new Set(
+			store.campaign.presentation.currentSlide?.beacons
+				.map((beacon) => beacon.host?.current.computedName)
+				.filter((h) => h) as string[]
+		)
+	);
 
 	return (
 		<div css={{ padding: '2px 1rem' }}>
-			<Header withMargin>
-				{beacons && beacons.length === 1 ? beacons[0].computedNameWithHost : <Txt italic>Multiple Beacons</Txt>}
+			<Header withMargin css={{ display: 'flex', flexWrap: 'wrap' }}>
+				<Txt>
+					<PresentationHeaderPart names={hostNames} type="Host" />
+				</Txt>
+				<Spacer>/</Spacer>
+				<Txt normal>
+					<PresentationHeaderPart names={beaconNames} type="Beacon" />
+				</Txt>
 			</Header>
-			<Txt block monospace cy-test="slide-header">
+			<Txt block monospace muted cy-test="slide-header">
 				{store.campaign.presentation.currentSlide?.minDate ? state.min.format(`ddd ${dateTimeFormat}`) : 'Unknown'}
 				{store.campaign.presentation.currentSlide?.commandIds?.length! > 1 && (
 					<>
@@ -40,3 +56,31 @@ export const PresentationItemHeader = observer<PresentationItemHeaderProps>(({})
 		</div>
 	);
 });
+
+const PresentationHeaderPart = ({ names, type, ...props }: TxtProps & { names?: string[]; type: string }) => (
+	<>
+		{names == null ? (
+			<Txt disabled {...props}>
+				Unknown {type}
+			</Txt>
+		) : names.length === 1 ? (
+			<Txt {...props}>{names[0]}</Txt>
+		) : (
+			<Tooltip
+				placement="bottom-start"
+				interactionKind="hover"
+				content={
+					<ul css={{ margin: 0, padding: 0, listStyle: 'none' }}>
+						{names.map((name) => (
+							<li>{name}</li>
+						))}
+					</ul>
+				}
+			>
+				<Txt italic {...props}>
+					Multiple {type}s
+				</Txt>
+			</Tooltip>
+		)}
+	</>
+);
