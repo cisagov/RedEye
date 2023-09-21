d383442ff3b3c6516edf5d248bf6bc38a8afeddc
Huiqing Li <forqing@gmail.com>
2023-09-21 10:50:05 -0700

    revert astro config for repo

--- docs/landing-page/astro.config.mjs
+++ docs/landing-page/astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/RedEye' /* use for gh-pages build */,
+	// base: '/RedEye' /* use for gh-pages build */,
 	outDir: './dist-landing',
 	vite: {
 		build: {
